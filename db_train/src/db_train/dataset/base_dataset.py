55b6274d17de6224d201324ea6b231798ef3c2b3
dBHz01 <928300456@qq.com>
2024-03-01 11:30:20 +0800

    feat(base_dataset): Add mode numpy.

--- db_train/src/db_train/dataset/base_dataset.py
+++ db_train/src/db_train/dataset/base_dataset.py
@@ -1,15 +1,17 @@
+import numpy as np
 from torch.utils.data import Dataset
 from abc import ABC, abstractmethod
 
 class BaseDataset(Dataset, ABC):
 
-  MODE_LIST: list[str] = ['list', 'tensor']
+  MODE_LIST: list[str] = ['list', 'tensor', 'numpy']
 
   def __init__(self, mode: str = 'list') -> None:
     assert mode in self.MODE_LIST
     self.mode = mode
     self.data: list = []
     self.tensors: list = []
+    self.np_array: np.ndarray = np.array([])
 
   @abstractmethod
   def load(self) -> None:
@@ -28,9 +30,13 @@ class BaseDataset(Dataset, ABC):
       return len(self.data)
     elif self.mode == 'tensor':
       return self.tensors[0].size(0)
+    elif self.mode == 'numpy':
+      return self.np_array.shape[0]
 
   def __getitem__(self, index: int):
     if self.mode == 'list':
       return self.data[index]
     elif self.mode == 'tensor':
       return tuple(tensor[index] for tensor in self.tensors)
+    elif self.mode == 'numpy':
+      return self.np_array[index]
